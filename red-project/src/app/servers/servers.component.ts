302898958cfa570f2210dce1365d644cba2b2a5a
Daniel Viveros <dviveros90@gmail.com>
2023-02-21 00:36:11 -0500

    Changing selector template name
    It can be used by attribute or class in div

--- red-project/src/app/servers/servers.component.ts
+++ red-project/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // As a div attribute selector: '[app-servers]',
+  // As a div class selector: '.app-servers',
   selector: 'app-servers',
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
