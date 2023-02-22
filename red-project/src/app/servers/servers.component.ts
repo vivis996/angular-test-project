d787d5168d63624308417922a86244685a6f8d9f
Daniel Viveros <dviveros90@gmail.com>
2023-02-22 17:04:22 -0500

    List of server was added
    Using ngFor

--- red-project/src/app/servers/servers.component.ts
+++ red-project/src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent {
   serverCreationStatus: string = "No server was created!";
   serverName: string = "TestServer";
   serverCreated: boolean = false;
+  servers = [ 'Test server', 'Test server 2', ];
 
   constructor(){
     setTimeout(() => {
@@ -21,6 +22,7 @@ export class ServersComponent {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
 
