49aa70988fdf5f1f9e780b03e3acc8616021cf86
Daniel Viveros <dviveros90@gmail.com>
2023-02-24 18:47:32 -0500

    App base for introduction

--- red-project/src/app/app.component.ts
+++ red-project/src/app/app.component.ts
@@ -3,7 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  serverElements = [
+    {
+      type: 'server',
+      name: 'Server test',
+      content: 'This is a server test',
+    },
+    {
+      type: 'blueprint',
+      name: 'Blueprint test',
+      content: 'This is a blueprint test',
+    },
+  ];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
 }
--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
