b1656d38537e52e01457872f40d149236eaa0592
Daniel Viveros <dviveros90@gmail.com>
2023-02-24 19:05:36 -0500

    App was separated in components
    App is not working yet

--- red-project/src/app/app.component.ts
+++ red-project/src/app/app.component.ts
@@ -18,22 +18,4 @@ export class AppComponent {
       content: 'This is a blueprint test',
     },
   ];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
 }
--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule,
