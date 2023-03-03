1dcabc5a8e95923a2903a0bf02231ab63392f47c
Daniel Viveros <dviveros90@gmail.com>
2023-03-03 14:56:39 -0500

    View encapsulation
    Change the behavior of the css

--- red-project/src/app/server/server.component.ts
+++ red-project/src/app/server/server.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css'],
+  // encapsulation: ViewEncapsulation.None,
 })
 export class ServerComponent {
   @Input("srvElement") element: {
