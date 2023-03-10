14eaadd14c0c6338f05887564a8ab7e0e96d0344
Daniel Viveros <dviveros90@gmail.com>
2023-03-10 14:58:10 -0500

    Creating a Basic Attribute Directive

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BasicHighLightDirective,
   ],
   imports: [
     BrowserModule,
