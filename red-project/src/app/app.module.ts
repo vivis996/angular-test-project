bed99709c5ab4b441cf6fdd70bcf2c94a1967569
Daniel Viveros <dviveros90@gmail.com>
2023-03-10 16:44:42 -0500

    Building a Structural Directive

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BasicHighLightDirective } from './directives/basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './directives/better-highlight/better-highlight.directive';
+import { UnlessDirective } from './directives/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighLightDirective,
     BetterHighlightDirective,
+    UnlessDirective,
   ],
   imports: [
     BrowserModule,
