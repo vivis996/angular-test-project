2f1d7c4fa839816804becf966254b5ac316b8a03
Daniel Viveros <dviveros90@gmail.com>
2023-03-15 15:20:08 -0400

    Using Services for Cross-Component Communication
    Using emit and subscribe

--- red-project/src/app/new-account/new-account.component.ts
+++ red-project/src/app/new-account/new-account.component.ts
@@ -7,7 +7,11 @@ import { AccountsService } from '../services/accounts.service';
   styleUrls: ['./new-account.component.css'],
 })
 export class NewAccountComponent {
-  constructor(private accountsService: AccountsService) {}
+  constructor(private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
