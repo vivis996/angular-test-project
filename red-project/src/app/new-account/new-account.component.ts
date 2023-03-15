42095a7063e037ee740fc6ae51e3144933c433e8
Daniel Viveros <dviveros90@gmail.com>
2023-03-15 13:42:55 -0400

    Injecting the Logging Service into Components

--- red-project/src/app/new-account/new-account.component.ts
+++ red-project/src/app/new-account/new-account.component.ts
@@ -5,15 +5,18 @@ import { LoggingService } from '../services/logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService],
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus,
     });
-    new LoggingService().logStatusChange(accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
