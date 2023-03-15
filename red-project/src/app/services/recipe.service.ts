43bb5ebaa5bd88d5e21437fecd9b15c9a775f093
Daniel Viveros <dviveros90@gmail.com>
2023-03-15 17:15:01 -0400

    Adding Ingredients to Recipes
    * I've already added that component in Section-3.
    * Only added quantity of ingredients and showed it.
    * Adding Ingredients to Recipes

--- red-project/src/app/services/recipe.service.ts
+++ red-project/src/app/services/recipe.service.ts
@@ -10,12 +10,12 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'Simple a test', 'https://bit.ly/3SnSwGZ', [
-      new Ingredient('egg', 0),
-      new Ingredient('tomato', 0),
+      new Ingredient('Egg', 2),
+      new Ingredient('Tomato', 1),
     ]),
     new Recipe('A test recipe 2', 'Simple a test 2', 'https://bit.ly/3IQH2ss', [
-      new Ingredient('onion', 0),
-      new Ingredient('avocado', 0),
+      new Ingredient('Onion', 1),
+      new Ingredient('Avocado', 3),
     ]),
   ];
 
