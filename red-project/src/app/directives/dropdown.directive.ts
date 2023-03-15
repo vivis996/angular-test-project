ae936b7e9ab0c33f5cf3d95eebb544870b9f36c1
Daniel Viveros <dviveros90@gmail.com>
2023-03-14 23:27:57 -0400

    Building and Using a Dropdown Directive
    Note: Style is only for Bootstrap 5.

---
+++ red-project/src/app/directives/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+ 
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+  constructor(private elRef: ElementRef) {}
+}
