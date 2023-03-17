59253c0c3610c9b5f1cc30c534f51437f29d7614
Daniel Viveros <dviveros90@gmail.com>
2023-03-17 18:10:26 -0400

    Passing Query Parameters and Fragments

--- red-project/src/app/home/home.component.ts
+++ red-project/src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class HomeComponent {
   constructor(private router: Router) {}
   
-  onLoadServers() {
-    this.router.navigate(['/servers']);
+  onLoadServer(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 }
