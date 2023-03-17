ef7ca09d64336bfb2fb3e666ffd83e5ad9bfa3d2
Daniel Viveros <dviveros90@gmail.com>
2023-03-17 01:21:34 -0400

    Fetching Route Parameters

--- red-project/src/app/users/user/user.component.ts
+++ red-project/src/app/users/user/user.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   user: { id: number; name: string };
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name'],
+    };
+  }
 }
