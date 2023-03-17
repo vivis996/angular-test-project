e222e4223353361147e3e6cc85dffd9d98c9a1c3
Daniel Viveros <dviveros90@gmail.com>
2023-03-17 18:28:18 -0400

    Practicing and some Common Gotchas

--- red-project/src/app/servers/server/server.component.ts
+++ red-project/src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,9 +11,13 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService) {}
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
+    this.route.params.subscribe(
+      (params: Params) => this.server = this.serversService.getServer(+params['id'])
+    );
   }
 }
