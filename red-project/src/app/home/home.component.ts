e4cedcca00c663d7522f641abae9cf90e54ff0d2
Daniel Viveros <dviveros90@gmail.com>
2023-03-17 23:13:23 -0400

    Using a Fake Auth Service

--- red-project/src/app/home/home.component.ts
+++ red-project/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private authService: AuthService) {}
   
   onLoadServer(id: number) {
     this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
