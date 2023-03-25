8c8c737f6d6ac6287c415604bfa3b4754036b609
Daniel Viveros <dviveros90@gmail.com>
2023-03-25 00:35:50 -0400

    Adding Navigation to the App

--- red-project/src/app/app.component.ts
+++ red-project/src/app/app.component.ts
@@ -6,9 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  featureLoaded: string = 'recipe';
-
-  onNavigate(feature: string) {
-    this.featureLoaded = feature;
-  }
 }
--- red-project/src/app/header/header.component.ts
+++ red-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-  headerSelected: string = 'recipe';
-
-  onSelect(feature: string) {
-    this.headerSelected = feature;
-    this.featureSelected.emit(feature);
-  }
 }
