e081cd3b2e626a04f7465bc5e17feccafcda1f9e
Daniel Viveros <dviveros90@gmail.com>
2023-03-26 14:05:12 -0400

    Errors & Completion

--- red-project/src/app/home/home.component.ts
+++ red-project/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription, Observable } from 'rxjs';
+import { interval, Subscription, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,16 +13,26 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.firstObsSuscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // });
-    const customIntervalObservable = Observable.create((observer) => {
+    const customIntervalObservable = Observable.create((observer: Observer<number>) => {
       let count = 0;
-      setInterval(() => {        
+      setInterval(() => {
         observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSuscription = customIntervalObservable.subscribe(data => {
+    this.firstObsSuscription = customIntervalObservable.subscribe((data: number) => {
       console.log(data);
+    }, (error: any) => {
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
