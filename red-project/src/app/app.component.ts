f8f5ce0dd26d28febcc50fe8e835b85e8c4ed5f0
Daniel Viveros <dviveros90@gmail.com>
2023-03-26 14:23:08 -0400

    Subjects

--- red-project/src/app/app.component.ts
+++ red-project/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated: boolean = false;
+  private activatedSub: Subscription;
+  
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivated => this.userActivated = didActivated);
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 }
