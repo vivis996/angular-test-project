eaa819cd6e2f1d4c90abcf06707e92bc6504228e
Daniel Viveros <dviveros90@gmail.com>
2023-04-13 21:47:52 -0400

    Allowing the Selection of Items in the List

--- red-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ red-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, } from '@angular/core';
+import { Component, OnDestroy, OnInit, } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -8,9 +9,25 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css'],
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  suscription: Subscription;
+  editMode = false;
+  editItemIndex: number;
 
   constructor(private slService: ShoppingListService) {}
+
+  ngOnInit(): void {
+    this.suscription = this.slService.startEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editItemIndex = index;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.suscription.unsubscribe();
+  }
 
   onAddItem(form: NgForm) {
     const ingredient = new Ingredient(form.value.name, Number(form.value.amount));
--- red-project/src/app/shopping-list/shopping-list.component.ts
+++ red-project/src/app/shopping-list/shopping-list.component.ts
@@ -11,18 +11,22 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
   private subscription: Subscription;
-
+  
   constructor(private slService: ShoppingListService) {}
-
+  
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
     this.subscription = this.slService.ingredientsChanged
-      .subscribe(
-        (ingredients: Ingredient[]) => this.ingredients = ingredients
+    .subscribe(
+      (ingredients: Ingredient[]) => this.ingredients = ingredients
       );
-  }
+    }
+    
+    ngOnDestroy(): void {
+      this.subscription.unsubscribe();
+    }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    onEditItem(index: number): void {
+      this.slService.startEditing.next(index);
+    }
   }
-}
