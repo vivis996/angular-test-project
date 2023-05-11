87e806bf2e9007df603a20c36ce94f59600aec18
Daniel Viveros <dviveros90@gmail.com>
2023-05-11 15:35:07 -0400

    Using RxJS Operators to Transform Response Data
    - Add map operator to transform response data
    - Improve readability of code

--- red-project/src/app/app.component.ts
+++ red-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -25,10 +26,14 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts(){
-    this.http.get(this.urlDatabaseFireBase + 'posts.json')
-            .subscribe(posts => {
-              console.log(posts);
-            });
+    this.http
+      .get(this.urlDatabaseFireBase + 'posts.json')
+      .pipe(map(responseData =>{
+        return Object.entries(responseData).map(([id, post]) => ({ ...post, id}));
+      }))
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 
   onFetchPosts() {
