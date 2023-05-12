4dace68cdb4e6b2f5157253bfb104f3129cda128
Daniel Viveros <dviveros90@gmail.com>
2023-05-12 18:40:06 -0400

    Setting up the DataStorage Service
    - Add HttpClientModule to app.module.ts
    - Create DataStorageService with HttpClient in shared folder

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -33,6 +34,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
