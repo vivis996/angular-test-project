39f5152e0db4d3022824f1c1273a782b5cd5742f
Daniel Viveros <dviveros90@gmail.com>
2023-05-12 18:57:44 -0400

    Storing Recipes
    - Added click event listener to "Save data" dropdown item
    - Created onSaveData() method in HeaderComponent
    - Imported DataStorageService into HeaderComponent
    - Defined constructor for DataStorageService in HeaderComponent
    - Defined storeRecipes() method in DataStorageService to send HTTP PUT request with recipes data

--- red-project/src/app/shared/data-storage.service.ts
+++ red-project/src/app/shared/data-storage.service.ts
@@ -1,9 +1,18 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../services/recipe.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  private urlDatabaseFirebase: string = 'https://angular-the-complete-gui-3c98a-default-rtdb.firebaseio.com/';
+  private endPoint: string = 'recipes.json';
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.urlDatabaseFirebase + this.endPoint, recipes)
+      .subscribe(response =>  console.log(response));
+  }
 }
