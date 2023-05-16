c1629de3f15cb882384ded020425085eb92903fc
Daniel Viveros <dviveros90@gmail.com>
2023-05-15 20:46:46 -0400

    Transforming Response Data
    - Commented out initial recipes array
    - Added map operator to fetchRecipes method in data-storage service
    - Map operator transforms response data by checking if ingredients exist and returning an empty array if not.

--- red-project/src/app/services/recipe.service.ts
+++ red-project/src/app/services/recipe.service.ts
@@ -10,16 +10,18 @@ import { ShoppingListService } from './shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Simple a test', 'https://bit.ly/3SnSwGZ', [
-      new Ingredient('Egg', 2),
-      new Ingredient('Tomato', 1),
-    ]),
-    new Recipe('A test recipe 2', 'Simple a test 2', 'https://bit.ly/3IQH2ss', [
-      new Ingredient('Onion', 1),
-      new Ingredient('Avocado', 3),
-    ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('A test recipe', 'Simple a test', 'https://bit.ly/3SnSwGZ', [
+  //     new Ingredient('Egg', 2),
+  //     new Ingredient('Tomato', 1),
+  //   ]),
+  //   new Recipe('A test recipe 2', 'Simple a test 2', 'https://bit.ly/3IQH2ss', [
+  //     new Ingredient('Onion', 1),
+  //     new Ingredient('Avocado', 3),
+  //   ]),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
--- red-project/src/app/shared/data-storage.service.ts
+++ red-project/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../services/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +20,11 @@ export class DataStorageService {
 
   fetchRecipes(): void {
     this.http.get<Recipe[]>(this.urlDatabaseFirebase + this.endPoint)
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
