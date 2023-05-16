95a989db6f163633350fad882b8f08208ce1389f
Daniel Viveros <dviveros90@gmail.com>
2023-05-16 18:00:00 -0400

    Preparing the Signup Request
    - Added AuthResponseData interface to handle authentication response data
    - Added AuthService class to handle user authentication through HTTP requests.

---
+++ red-project/src/app/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { AuthResponseData } from "./auth.model";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  urlAuth: string = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+  apiKey: string = '[AIzaSyCQX7qVP2cpW7x9ctJKG48OKOFOLhTrahU]';
+  
+  constructor(private http: HttpClient)
+  {}
+  
+  signup(email: string, password: string): Observable<AuthResponseData> {
+    return this.http.post<AuthResponseData>(this.urlAuth + this.apiKey,{
+      email,
+      password,
+      returnSecureToken: true,
+    });
+  }
+}
