405bab8ff5ea460f172c1eda38421009d759076e
Daniel Viveros <dviveros90@gmail.com>
2023-05-16 21:26:21 -0400

    Adding Auto-Login
    - Add OnInit interface and AuthService import
    - Create constructor with AuthService injection
    - Implement ngOnInit method that calls autoLogin from AuthService
    - Add autoLogin method to AuthService that retrieves user data from local storage and sets the user if token is valid. 
    - Update handleAuthentication method in AuthService to store user data in local storage after successful login.

--- red-project/src/app/auth/auth.service.ts
+++ red-project/src/app/auth/auth.service.ts
@@ -37,6 +37,24 @@ export class AuthService {
     }).pipe(catchError(this.handleError), tap(response => this.handleAuthentication(response)));
   }
 
+  autoLogin(): void {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+    if (!loadedUser.token) {
+      return;
+    }
+    this.user.next(loadedUser);
+  }
+
   logout(): void {
     this.user.next(null);
     this.router.navigate(['./auth']);
@@ -46,6 +64,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + (Number(response.expiresIn) * 1000));
     const user = new User(response.email, response.localId, response.idToken, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse): Observable<never> {
