f0aa4bf85f14579e1dee5724fc90ad6aa7f003c0
Daniel Viveros <dviveros90@gmail.com>
2023-05-17 17:31:41 -0400

    Data Binding & Event Binding
    - Add OnDestroy to AuthComponent
    - Add Subscription to closeSub in AuthComponent
    - Remove error property from AuthComponent
    - Update showErrorAlert method in AuthComponent to handle AlertComponent instance and subscription
    - Update alert.component.html to include a heading for the error message.

--- red-project/src/app/auth/auth.component.ts
+++ red-project/src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { AuthResponseData } from "./auth.model";
 import { Router } from "@angular/router";
 import { AlertComponent } from "../shared/alert/alert.component";
@@ -11,11 +11,11 @@ import { PlaceholderDirective } from "../shared/placeholder/placerholder.directi
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode: boolean = true;
   isLoading: boolean = false;
-  error: string = null;
   @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
+  private closeSub: Subscription;
 
   constructor(private authService: AuthService,
     private router: Router,
@@ -34,7 +34,6 @@ export class AuthComponent {
     const password = form.value.password;
     
     this.isLoading = true;
-    this.error = null;
     
     let authObs: Observable<AuthResponseData>;
     if (this.isLoginMode) {
@@ -49,7 +48,6 @@ export class AuthComponent {
         this.router.navigate(['/recipes']);
       },
       errorMessage => {
-        this.error = errorMessage;
         this.showErrorAlert(errorMessage);
         this.isLoading = false;
       });
@@ -57,14 +55,21 @@ export class AuthComponent {
     form.reset();
   }
 
-  onHandleError(): void {
-    this.error = null;
+  ngOnDestroy(): void {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 
   private showErrorAlert(errorMessage: string): void {
     const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
     const hostViewContainerRef = this.alertHost.viewContainerRef;
     hostViewContainerRef.clear();
-    hostViewContainerRef.createComponent(alertCmpFactory);
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+    componentRef.instance.message = errorMessage;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
