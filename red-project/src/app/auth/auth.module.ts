552f453a7f61e214bdf40f85ec48e6da15816c39
Daniel Viveros <dviveros90@gmail.com>
2023-05-18 20:11:23 -0400

    Adding an Auth Feature Module
    - Remove '/auth' path from appRoutes in app-routing.module.ts
    - Add AuthModule to imports array in app.module.ts
    - Create AuthModule with declarations, imports, and route configuration in auth/auth.module.ts

---
+++ red-project/src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { AuthComponent } from "./auth.component";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+  ],
+  imports: [
+    SharedModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'auth', component: AuthComponent, },
+    ]),
+  ],
+})
+export class AuthModule {
+}
