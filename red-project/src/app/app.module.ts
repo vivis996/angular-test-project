66f3085825e146dc6181888293d26ebeb6252107
Daniel Viveros <dviveros90@gmail.com>
2023-05-21 00:38:28 -0400

    More Lazy Loading
    - Add lazy loading for shopping list and auth modules
    - Remove import statements from app module
    - Update routing paths in auth and shopping list modules

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -7,10 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -23,10 +21,8 @@ import { AuthModule } from './auth/auth.module';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule,
-    AuthModule,
   ],
   bootstrap: [AppComponent],
 })
--- red-project/src/app/auth/auth.module.ts
+++ red-project/src/app/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { SharedModule } from "../shared/shared.module";
     SharedModule,
     FormsModule,
     RouterModule.forChild([
-      { path: 'auth', component: AuthComponent, },
+      { path: '', component: AuthComponent, },
     ]),
   ],
 })
