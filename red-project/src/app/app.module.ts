d43484a342df6da0c0a4e7e217dfce6d83735418
Daniel Viveros <dviveros90@gmail.com>
2023-05-21 00:31:41 -0400

    Implementing Lazy Loading
    - Moved recipes module to lazy loading
    - Updated app-routing.module.ts and recipes-routing.module.ts
    - Removed RecipesModule from app.module.ts

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -24,7 +23,6 @@ import { AuthModule } from './auth/auth.module';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
--- red-project/src/app/recipes/recipes-routing.module.ts
+++ red-project/src/app/recipes/recipes-routing.module.ts
@@ -10,7 +10,7 @@ import { AuthGuard } from "../auth/auth.guard";
 import { RecipeResolverService } from "./recipes-resolver.service";
 
 const recipesRoutes: Routes = [
-  { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard],
+  { path: '', component: RecipesComponent, canActivate: [AuthGuard],
     children: [
       { path: '', component: RecipeStartComponent, },
       { path: 'new', component: RecipeEditComponent, },
