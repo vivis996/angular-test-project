83452aea0065e52af94b27323221fbe2e38cb383
Daniel Viveros <dviveros90@gmail.com>
2023-05-31 02:23:42 +0100

    Building a First Standalone Component
    - Update angular.json with analytics configuration
    - Remove SharedModule from App module imports
    - Update Detail component imports with SharedModule
    - Add standalone true to Detail Component selector
    - Import Detail Component in Welcome Component

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { DetailsComponent } from './welcome/details/details.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { DetailsComponent } from './welcome/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule,
+    DetailsComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- red-project/src/app/welcome/details/details.component.ts
+++ red-project/src/app/welcome/details/details.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
+  imports: [SharedModule],
+  standalone: true,
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
