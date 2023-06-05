cae4fa175ed199616d50db28ceb625519c99dba9
Daniel Viveros <dviveros90@gmail.com>
2023-06-05 04:21:15 +0100

    Services & Standalone Components

--- red-project/src/main.ts
+++ red-project/src/main.ts
@@ -1,4 +1,7 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { AnalyticsService } from "./app/shared/analytics.service";
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [AnalyticsService,]
+});
