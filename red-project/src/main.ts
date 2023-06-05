ebfa307380997ed2d00df4f5507733e3da227097
Daniel Viveros <dviveros90@gmail.com>
2023-06-05 04:22:31 +0100

    Returning mode (last one was only an example)

--- red-project/src/main.ts
+++ red-project/src/main.ts
@@ -1,7 +1,4 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
-import { AnalyticsService } from "./app/shared/analytics.service";
 
-bootstrapApplication(AppComponent, {
-  providers: [AnalyticsService,]
-});
+bootstrapApplication(AppComponent);
