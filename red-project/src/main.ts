1ed5099a184291accec59cd244cfadb02ce2c1fa
Daniel Viveros <dviveros90@gmail.com>
2023-06-07 19:38:00 -0400

    Routing with Standalone Components

--- red-project/src/main.ts
+++ red-project/src/main.ts
@@ -1,9 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
 
 bootstrapApplication(AppComponent, {
   providers: [
     // AnalyticsService,
+    importProvidersFrom(AppRoutingModule),
   ],
 });
