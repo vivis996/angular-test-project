063d6f9e9c87e23a81262f86eec2341fdd8bf738
Daniel Viveros <dviveros90@gmail.com>
2023-06-10 19:11:59 -0400

    Config settings & cleaning code

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
