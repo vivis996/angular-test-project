1ffd867e7d2dd57839f70f5770e4090228e26b69
Daniel Viveros <dviveros90@gmail.com>
2023-06-10 20:08:39 -0400

    Project Setup & Installing NgRx

--- red-project/src/app/app.module.ts
+++ red-project/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { CounterControlsComponent } from './counter-controls/counter-controls.co
     CounterOutputComponent,
     CounterControlsComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
